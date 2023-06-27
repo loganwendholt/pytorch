67e04deb90aa2fe95a334ea1b1a1cc5df6b1bd2b
XiaobingSuper <xiaobing.zhang@intel.com>
2023-06-27 00:30:48 -0400

    inductor: move the CPU weight packing path to after of AOTAutograd
    ghstack-source-id: 6af461edb9af77279f35cb1db68a5d3374ae7e5b
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/103851

--- torch/_inductor/freezing.py
+++ torch/_inductor/freezing.py
@@ -8,6 +8,9 @@ import torch
 import torch.fx.traceback as fx_traceback
 import torch.utils._pytree as pytree
 from torch._dynamo.utils import detect_fake_mode
+
+from torch._functorch.compile_utils import fx_graph_cse
+from torch._inductor.fx_passes.freezing_patterns import freezing_passes
 from torch.ao.quantization._pt2e.utils import _fuse_conv_bn_
 from torch.fx.experimental.proxy_tensor import make_fx
 from . import config
@@ -46,32 +49,24 @@ def replace_node_with_constant(gm, node, constant):
 def replace_params_with_constants(gm, flat_params, fw_metadata) -> List[int]:
     """
     Replaces the parameters of a PyTorch GraphModule with constants wherever possible.
-
     Returns a list of indices representing the input parameters that were not converted to constants.
     """
-
     params = [node for node in gm.graph.nodes if node.op == "placeholder"]
     fake_inp_nodes = params[: len(params)]
-
     g = gm.graph
-
     preserved_arg_indices = []
     aliased_input_args = [
         out_info.base_idx
         for out_info in fw_metadata.output_info
         if out_info.base_idx is not None
     ]
-
     for i, (real_input, node) in enumerate(zip(flat_params, fake_inp_nodes)):
         if i in fw_metadata.mutated_inp_indices or aliased_input_args:
             preserved_arg_indices.append(i)
             continue
-
         replace_node_with_constant(gm, node, real_input)
-
     # add on non param inputs
     preserved_arg_indices.extend(range(len(flat_params), len(params)))
-
     # is this necessary ?
     gm.recompile()
     return preserved_arg_indices
@@ -219,6 +214,15 @@ def freeze(
         aot_autograd_gm, example_inputs, preserved_arg_indices
     )
 
+    # TODO - further restrict cse ? right now needed to dedup aliasing ops
+    cse_graph = fx_graph_cse(aot_autograd_gm.graph)
+    aot_autograd_gm.graph = cse_graph
+    aot_autograd_gm.recompile()
+    freezing_passes(aot_autograd_gm)
+
+    # TODO - apply legalization in pattern matcher
+    torch.fx.passes.tools_common.legalize_graph(aot_autograd_gm)
+    constant_fold(aot_autograd_gm)
     # invalidate nn Modules
     if config.freezing_discard_parameters:
         invalidate_eager_modules()
